2dd9d057845dfe274a47603b2f083690993d38d3
krieger27 <krieger@vols.utk.edu>
2017-12-19 15:28:39 -0600

    breaddomprices

--- javascripts/dominteractions.js
+++ javascripts/dominteractions.js
@@ -7,17 +7,27 @@ let cheese = document.getElementById("cheese");
 let condiments = document.getElementById("condiments");
 let meat = document.getElementById("meat");
 let veggies = document.getElementById("veggies");
+let output = document.getElementById("output");
 let sandwichMaker= require("./sandwichMaker");
 
 //For submit button
 submit.addEventListener('click', function(){
-    console.log('button clicked');
+    output.innerHTML= `${sandwichMaker.getTotal()} ${sandwichOutput(sandwichMaker.getSandwich())}`;
 });
 
-menu.addEventListener("click", function(){
+menu.addEventListener("change", function(){
     sandwichMaker.addIngredient(event.target.closest("div").id, event.target.value);
 });
 
-
+let sandwichOutput = function (sandwichObject){
+    let sandwichString = "";
+    for(let ingredient in sandwichObject) {
+        console.log(sandwichObject[ingredient]);
+        for(let i =0; i <sandwichObject[ingredient].length; i ++){
+            sandwichString += `${sandwichObject[ingredient][i]} `;
+        }
+    }
+    return sandwichString;
+};
 
 
--- javascripts/sandwichMaker.js
+++ javascripts/sandwichMaker.js
@@ -4,9 +4,25 @@ require("./dominteractions");
 let bread = require("./bread");
 let total = 0;
 
+let sandwich = {
+    bread: [],
+    cheese: [],
+    meat: [],
+    condiments: [],
+    veggies:[]
+};
 
 module.exports.addIngredient = function (id, value){
-    total +=bread.addBread.value;
+    sandwich[id].push(value);
+    // console.log(sandwich);
+    total +=bread.addBread(value);
     console.log(total);
 };
 
+module.exports.getTotal = function(){
+    return total;
+};
+
+module.exports.getSandwich = function(){
+    return sandwich;
+};
