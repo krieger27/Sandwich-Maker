6bcb086c5aab1fce21ebfd484ca749751f1f2ff4
krieger27 <krieger@vols.utk.edu>
2017-12-19 13:07:17 -0600

    menueventlistener

--- javascripts/dominteractions.js
+++ javascripts/dominteractions.js
@@ -1,10 +1,23 @@
 "use strict";
 
-
-
+let menu = document.getElementById("menu");
 let submit = document.getElementById("submit");
+let bread = document.getElementById("bread");
+let cheese = document.getElementById("cheese");
+let condiments = document.getElementById("condiments");
+let meat = document.getElementById("meat");
+let veggies = document.getElementById("veggies");
 
+//For submit button
 submit.addEventListener('click', function(){
     console.log('button clicked');
 });
+
+menu.addEventListener("click", function(){
+    console.log(event.target.value);
+    console.log(event.target.closest("div").id);
+});
+
+
+
 
